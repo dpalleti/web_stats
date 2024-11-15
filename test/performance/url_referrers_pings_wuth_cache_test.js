a77110044f339fdbc6927e52853a8ac0f7120ce4
divijapalleti <divija@interviewbit.com>
2024-11-15 04:42:08 -0800

    added ccache tests

---
+++ test/performance/url_referrers_pings_wuth_cache_test.js
@@ -0,0 +1,40 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Trend } from 'k6/metrics';
+
+export let options = {
+    scenarios: {
+        steady_load: {
+            executor: 'constant-vus',
+            vus: 10, // variable values 10, 20, 50, 100
+            duration: '1m', // can be adjusted 30s, 1m, 3m, etc.
+        },
+        spike_test: {
+            executor: 'ramping-vus',
+            startVUs: 1,
+            stages: [
+                { duration: '30s', target: 5 },
+                { duration: '30s', target: 10 },
+                { duration: '30s', target: 40 },
+                { duration: '30s', target: 5 }, // Ramp down to 5
+            ],
+        },
+    },
+    thresholds: {
+        http_req_duration: ['p(95)<1000'], // 95% of requests should complete in < 1000ms
+    },
+};
+
+const responseTime = new Trend('response_time');
+
+export default function () {
+    const res = http.get('http://0.0.0.0:3000/top-referrers?use_cache=true');
+
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'response time < 1000ms': (r) => r.timings.duration < 1000,
+    });
+
+    responseTime.add(res.timings.duration);
+    sleep(1); // can be adjusted - 0.5s, 1s, or 2s
+}
---
+++ test/performance/url_views_with_cache_test.js
@@ -0,0 +1,40 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Trend } from 'k6/metrics';
+
+export let options = {
+    scenarios: {
+        steady_load: {
+            executor: 'constant-vus',
+            vus: 10, // variable values 10, 20, 50, 100
+            duration: '30s', // can be adjusted 30s, 1m, 3m, etc.
+        },
+        spike_test: {
+            executor: 'ramping-vus',
+            startVUs: 1,
+            stages: [
+                { duration: '30s', target: 5 },
+                { duration: '30s', target: 10 },
+                { duration: '30s', target: 40 },
+                { duration: '30s', target: 0 }, // Ramp down to 0
+            ],
+        },
+    },
+    thresholds: {
+        http_req_duration: ['p(95)<500'], // 95% of requests should complete in < 500ms
+    },
+};
+
+const responseTime = new Trend('response_time');
+
+export default function () {
+    const res = http.get('http://0.0.0.0:3000/top-urls?use_cache=true');
+
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'response time < 500ms': (r) => r.timings.duration < 500,
+    });
+
+    responseTime.add(res.timings.duration);
+    sleep(1); // can be adjusted - 0.5s, 1s, or 2s
+}
